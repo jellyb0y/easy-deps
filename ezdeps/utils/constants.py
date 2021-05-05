19933e0e4cb8dbd5d96679f4595aa6d231eebcd0
v.kisel <v.kisel@corp.mail.ru>
2021-05-05 15:59:34 +0300

    feat(cli): update

--- ezdeps/utils/constants.py
+++ ezdeps/utils/constants.py
@@ -4,6 +4,6 @@ from os import getcwd
 ROOT_DIR = Path(__file__).resolve().parent.parent
 CUR_DIR = getcwd()
 
-DOCS_PATH = f'{ROOT_DIR}/resources/docs.md'
+DOCS_PATH = f'{ROOT_DIR}/resources/docs.txt'
 PACKAGE_FILE = f'{CUR_DIR}/.ezdeps.json'
 RC_FILE = f'{CUR_DIR}/.ezdepsrc'
